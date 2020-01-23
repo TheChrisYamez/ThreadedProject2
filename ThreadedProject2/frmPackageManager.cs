952d36994ba6e531ab8fece698b5afdb3b9e423e
Brian(sait) <30776841+appleton6509@users.noreply.github.com>
2020-01-23 13:07:52 -0700

    Add project files.

---
+++ ThreadedProject2/frmPackageManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DBConnector;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadedProject2
+{
+    public partial class frmPackageManager : Form
+    {
+        public frmPackageManager()
+        {
+            InitializeComponent();
+        }
+    }
+}

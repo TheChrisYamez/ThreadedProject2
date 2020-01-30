e6648d791ad24bc18400d7dadd0ec9fd12e27fea
Stephen J. Sander <519464@ACDM.DS.SAIT.CA>
2020-01-30 13:56:36 -0700

    started making productDB

---
+++ DBConnector/TravelExpertsDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBConnector
+{
+    public static class TravelExpertsDB
+    {
+            public static SqlConnection GetConnection()
+            {
+                string connectionString = @"Data Source=SAIT224915\SQLEXPRESS;Initial Catalog=TravelExperts;Integrated Security=True";
+                SqlConnection con = new SqlConnection(connectionString);
+                return con;
+            }
+        }
+    }
+}

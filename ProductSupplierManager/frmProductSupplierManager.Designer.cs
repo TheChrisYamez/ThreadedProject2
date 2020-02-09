b5b6bdbc4aafc5fde6f27798577c5080e5407a0e
Chris Yamez <ChrisYamez@outlook.com>
2020-02-08 23:35:28 -0700

    .

--- ProductSupplierManager/frmProductSupplierManager.Designer.cs
+++ ProductSupplierManager/frmProductSupplierManager.Designer.cs
@@ -39,9 +39,9 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(60, 41);
+            this.comboBox1.Location = new System.Drawing.Point(60, 40);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(192, 24);
             this.comboBox1.TabIndex = 0;
             // 
             // dataGridView1

a509382eb9d8f5523d0ca3123ff5c710355f6cdc
Chris Yamez <ChrisYamez@outlook.com>
2020-02-09 19:28:47 -0700

    .

---
+++ ProductSupplierManager/ProductSupplier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSupplierManager
+{
+    class ProductSupplier
+    {
+        public ProductSupplier() { }
+
+        public int ProductSupplierId { get; set; }
+
+        public string SuppliedID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ ProductSupplierManager/ProductSupplierDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ProductSupplierManager
+{
+    class ProductSupplierDB
+    {
+        public  static List<ProductSupplier> GetProductSupplier()
+        {
+            List<ProductSupplier> productSuppliers = new List<ProductSupplier>();
+            ProductSupplier supplier = null;
+            SqlConnection con = 
+        }
+    }
+}
